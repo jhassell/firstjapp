e80cb9ef7e23d788232f021068341a87442c3e52
jhassell <hassell@ZigBeef.com>
2014-08-08 15:38:07 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstjapp::Application.config.secret_key_base = '601724939ef471d0a342db8c945abed88cec16af7cf846c14b2210772e78a44b4326084962b864041efc9d12e40015438868ac149fc93b5e436b3bec63d39c67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Firstjapp::Application.config.session_store :cookie_store, key: '_firstjapp_session'
